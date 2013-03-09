07ca37831077296de77b890cda37a75bc9cd9a77
Nadav Ivgi <git@shesek.info>
2013-03-09 15:37:41 +0200

    updated readme

--- USAGE.js
+++ USAGE.js
@@ -1,16 +1,14 @@
 var express = require('express')
   , subroute = require('express-subroute');
+  , app = express();
 
-
-var app = express();
 subroute.install(app); // or require('express-subroute').install() to enable on all express apps
-
 
 // or `subroute(app, '/foo', function() {... })` without `install()`ing
 app.subroute('/foo', function() {
   this.get(function(req, res) {}); // GET /foo
   this.post(function(req, res) {}); // POST /foo
-  this.get('/bar', function(req, res) {}); GET /foo/bar
+  this.get('/bar', function(req, res) {}); // GET /foo/bar
 
   this.subroute('/baz', function() {
     this.use(...); // -> .use('/foo/baz', ...)
